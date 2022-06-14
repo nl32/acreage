c997c727e40ca954151bcb0ecef974140a819031
Ethan Bickel <notethanbickel@gmail.com>
2022-06-14 13:43:14 -0500

    Replace template content with own files

---
+++ src/main/java/io/github/nl32/acreage/Acreage.java
@@ -0,0 +1,17 @@
+package io.github.nl32.acreage;
+
+import com.mojang.brigadier.tree.CommandNode;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class Acreage implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
